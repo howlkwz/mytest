b721753274a7fd460b7dbbf9b678662fcfd3023b
kwz <1911671294@qq..com>
2018-09-26 18:52:20 +0800

    sdfsac SDV

--- demo-test/src/main/java/cn/itheima/test/DemoTest.java
+++ demo-test/src/main/java/cn/itheima/test/DemoTest.java
@@ -3,6 +3,5 @@ package cn.itheima.test;
 public class DemoTest {
     public static void main(String[] args) {
         System.out.println("dfasfasdfasf");
-        System.out.println("dfeterf");
     }
 }
